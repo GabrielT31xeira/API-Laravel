8f63a11a4d7e25ac238db7bd98ed8602fd96eecd
Teixeira <gt3ixeira@gmail.com>
2021-02-02 19:14:26 -0300

    configure react.js

---
+++ frontend/src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/login';
+import Register from './pages/Register';
+
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" component={Logon}/>
+                <Route path="/regiter" component={Register}/>
+            </Switch>
+        </BrowserRouter>
+    );
+}
